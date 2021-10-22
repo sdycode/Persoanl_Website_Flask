749cf8e5155d787bb9d27f2bcc907bd1bc22f4b4
sdycode <sdycode@gmail.com>
2021-10-22 20:59:11 +0530

    Done with Nav Bar

--- app.py
+++ app.py
@@ -1,22 +1,28 @@
-    
+
 from flask import Flask, request, render_template
 import pickle
 
 app = Flask(__name__)
 
-@app.route('/index.html')
+@app.route('/')
 def hello_world():
+    return render_template('navbar.html')
+
+@app.route('/index.html')
+def hello_worldwe():
     return render_template('index.html')
 
 @app.route('/chats.html')
 def chats():
     return render_template('chats.html')
 
-
-
 @app.route('/appList.html')
 def appList():
     return render_template('appList.html')
+
+@app.route('/navbar.html')
+def navbar():
+    return render_template('navbar.html')
     
 @app.route('/youtube.html')
 def youtube():
