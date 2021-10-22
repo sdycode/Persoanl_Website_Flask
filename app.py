a0337870ced2aa79e8a96eb8c0f42b39818916f3
sdycode <sdycode@gmail.com>
2021-10-22 22:06:10 +0530

    All assets are added

--- app.py
+++ app.py
@@ -4,29 +4,40 @@ import pickle
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
     return render_template('navbar.html')
+
 
 @app.route('/index.html')
 def hello_worldwe():
     return render_template('index.html')
 
+
 @app.route('/chats.html')
 def chats():
     return render_template('chats.html')
+
+@app.route('/musicplayer.html')
+def music():
+    return render_template('musicplayer.html')
+
 
 @app.route('/appList.html')
 def appList():
     return render_template('appList.html')
 
+
 @app.route('/navbar.html')
 def navbar():
     return render_template('navbar.html')
-    
+
+
 @app.route('/youtube.html')
 def youtube():
     return render_template('youtube.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
