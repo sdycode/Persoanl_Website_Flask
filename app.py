73f3a31963436c58528b870bb4aec84a9b942e2f
sdycode <sdycode@gmail.com>
2021-10-25 18:20:08 +0530

    Individual App html pages are added

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, request, render_template
 import pickle
 
@@ -7,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('navbar.html')
+    return render_template('index.html')
 
 
 @app.route('/index.html')
@@ -22,7 +21,22 @@ def chats():
 @app.route('/musicplayer.html')
 def music():
     return render_template('musicplayer.html')
+    
+@app.route('/receipies.html')
+def receipies():
+    return render_template('receipies.html')
 
+@app.route('/spacexcrew.html')
+def spacexcrew():
+    return render_template('spacexcrew.html')
+
+@app.route('/ymart.html')
+def ymart():
+    return render_template('ymart.html')
+
+@app.route('/weather.html')
+def weather():
+    return render_template('weather.html')
 
 @app.route('/appList.html')
 def appList():
